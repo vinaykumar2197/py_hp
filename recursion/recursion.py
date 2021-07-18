a673b4f90ac6c1de6efb87824c89dad1518b7936
Vinay <vinaymishra2197@gmail.com>
2021-07-18 18:31:57 +0530

    impl of The harmonic sum is the sum of reciprocals of the positive integers done

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -82,3 +82,17 @@ def sum_series(numbers):
 print(sum_series(6))
 print(sum_series(10))
 # -------------------------------------------------------------------------
+
+
+# Write a Python program to calculate the harmonic sum of n-1.
+# Note: The harmonic sum is the sum of reciprocals of the positive integers.
+def harmonic_sum(number):
+    if number ==1:
+        return 1
+    else:
+        return 1/number + harmonic_sum(number-1)
+
+
+print(harmonic_sum(7))
+print(harmonic_sum(4))
+# -------------------------------------------------------------------------
